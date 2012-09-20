956f5bea18d43bbb812711b72746986a2f4a4370
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-09-19 17:40:17 -0700

    connect to mongo

--- app.js
+++ app.js
@@ -3,33 +3,43 @@
  * Module dependencies.
  */
 
-var express = require('express')
+var mongo = require('mongodb')
+  , express = require('express')
   , http = require('http')
   , path = require('path')
   , routes = require('./routes')
 
-var app = express();
-
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+global.db = new mongo.Db('githubdata', new mongo.Server("127.0.0.1", 27017, {})),
+db.open(function (err) {
+  if (err) throw err;
+  console.log('connected to mongo');
+  start();
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+function start () {
+  var app = express();
 
-routes(app);
+  app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+  });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+  app.configure('development', function(){
+    app.use(express.errorHandler());
+  });
+
+  routes(app);
+
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+  });
+}
