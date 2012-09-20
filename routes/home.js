796a6a2faa18084732caf04af0c9587b75f04df5
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-09-19 18:22:17 -0700

    use aggregation for /

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,11 @@
  */
 
 exports.index = function(req, res, next){
-  repos.distinct('language', function (err, langs) {
+  //repos.distinct('language', function (err, langs) {
+
+  var group = {$group : {_id : "$language", count : {$sum : 1 } } };
+  var sort = {$sort : {count : -1}};
+  repos.aggregate(group, sort, function (err, langs) {
     if (err) return next(err);
     res.render('index', { langs: langs || [] });
   });
