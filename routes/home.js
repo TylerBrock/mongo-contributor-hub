792b6b04d3ee9663a37e89fe4ab84ecb531f4c81
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-09-19 21:14:57 -0700

    fix lang sort

--- routes/home.js
+++ routes/home.js
@@ -23,6 +23,11 @@ setInterval(function updateLangs () {
       console.error('updateLangs error: ', err);
       languages || (languages = []);
     }
+    languages.sort(function (a, b) {
+      if (a.count > b.count) return -1;
+      if (a.count < b.count) return 1;
+      return 0;
+    });
     langs = languages;
   }
 
