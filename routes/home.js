47b6a0d19c896f462dab51d39b81f0e7d3ccc4ff
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-09-19 18:19:27 -0700

    add lang route

--- routes/home.js
+++ routes/home.js
@@ -3,9 +3,22 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.index = function(req, res, next){
   repos.distinct('language', function (err, langs) {
+    if (err) return next(err);
     res.render('index', { langs: langs || [] });
   });
 };
 
+exports.lang = function (req, res, next) {
+  var lang = req.param('lang');
+
+  repos
+  .find({ 'language': lang }, { limit: 20, sort: { followers: 1 }})
+  .toArray(function (err, langs) {
+    if (err) return next(err);
+
+    // todo use a different view
+    res.render('index', { langs: langs || [] });
+  });
+}
