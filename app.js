954e547b0599c9aaf0b7fb682065483deecefc89
Tyler Brock <tyler.brock@gmail.com>
2012-09-19 22:42:31 -0400

    adding favicon

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function start () {
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
-    app.use(express.favicon());
+    app.use(express.favicon(__dirname + '/public/favicon.ico'));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
