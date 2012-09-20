52a2d63a5e0e6578d51dca53f61934ba44abdd8b
Bernie Hackett <bernie@10gen.com>
2012-09-19 22:39:46 -0400

    Convert date strings to timestamps.

--- datapull.py
+++ datapull.py
@@ -3,6 +3,7 @@
 import json
 import urllib.request
 
+from datetime import datetime
 from pprint import pprint
 
 import pymongo
@@ -17,6 +18,8 @@ coll = db.repos
 coll.ensure_index([('username', pymongo.ASCENDING),
                    ('name', pymongo.ASCENDING)], unique=True)
 
+fmt = "%Y-%m-%dT%H:%M:%S"
+
 page = 0
 
 while True:
@@ -28,9 +31,16 @@ while True:
 
     data = response.read()
     js = json.loads(data.decode())
+    # github returns an empty array for 'repositories'
+    # when there are no more results.
     if js['repositories']:
         for repo in js['repositories']:
+            # Convert date strings to real timestamps.
+            for field in ('created', 'created_at', 'pushed', 'pushed_at'):
+                # TODO: This sucks ass but skip timezone offsets for the time being.
+                repo[field] = datetime.strptime(repo[field].rsplit('-', 1)[0], fmt)
             try:
+                # Update so we get updated followers and watchers count.
                 res = coll.update({'username': repo['username'],
                                    'name': repo['name']},
                                   repo, upsert=True, safe=True)
