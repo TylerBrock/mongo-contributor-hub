e88dc2209a971e591cd725d283e4801dd5f0d5d7
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-09-25 08:59:10 -0700

    datapull.js

---
+++ scripts/datapull.js
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+/**
+ * Dependencies
+ */
+
+var request  = require('superagent');
+var mongo = require('mongodb');
+
+/**
+ * mongo URI
+ */
+
+var mongouri = process.env.MONGOLAB_URI || 'mongodb://localhost/githubdata';
+
+/**
+ * Github api URL
+ */
+
+var uri = 'https://api.github.com/legacy/repos/search/mongodb?start_page=';
+
+/**
+ * Establish mongo connection
+ */
+
+var repos, db;
+
+log('starting');
+mongo.Db.connect(mongouri, function (err, db_) {
+  if (err) throw err;
+
+  db = db_;
+  repos = db.collection('repos');
+  index(repos);
+
+  log('connected to mongo');
+
+  // start pulling data
+  page(0, function (err) {
+    log('disconnecting from mongo');
+
+    db.close(function () {
+      if (err) {
+        log(err);
+        return process.exit(1);
+      }
+
+      log('completed successfully');
+      process.exit(0);
+    });
+  });
+});
+
+/**
+ * Fetch a search result from Github by page number
+ * and upsert our database
+ *
+ * This will fire recursively until either Github
+ * returns an error or there are no more results.
+ *
+ * @param {Number} num
+ * @param {Function} cb
+ */
+
+function page (num, cb) {
+  var req = request.get(uri + num);
+  req.on('error', cb);
+  req.end(function (res) {
+    var pending = res.body && res.body.repositories && res.body.repositories.length;
+
+    if (!pending) return cb();
+
+    // upsert in parallel
+    res.body.repositories.forEach(function (repo) {
+      fix(repo);
+
+      var match = { username: repo.username, name: repo.name };
+      var options = { upsert: true, safe: true };
+
+      repos.update(match, repo, options, function (err, res) {
+        if (err) log(err);
+        --pending || page(num+1, cb);
+      })
+    });
+  })
+}
+
+/**
+ * Parse github date strings into real Dates
+ *
+ * @param {Object} doc
+ */
+
+function fix (doc) {
+  doc.created = new Date(doc.created);
+  doc.created_at = new Date(doc.created_at);
+  doc.pushed = new Date(doc.pushed);
+  doc.pushed_at = new Date(doc.pushed_at);
+}
+
+/**
+ * Log the given `msg`
+ *
+ * @param {String|Error} msg
+ */
+
+function log (msg) {
+  var prefix = String(new Date) + ': datapull:';
+  if (msg instanceof Error)
+    console.error(prefix, (msg.stack || '').replace(/\n/g, '\\n'));
+  else
+    console.log(prefix, msg);
+}
+
+/**
+ * ensure indexes
+ */
+
+function index (col) {
+  log('ensuring indexes');
+  col.ensureIndex({ username: 1, name: 1 }, { unique: true, safe: true }, function (err) {
+    if (err) log(err);
+    else log('ensuring indexes complete');
+  });
+}
